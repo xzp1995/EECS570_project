0a2fe634c48636dc0ab20e7f1d12bc94615a6fbb
Zhipeng Xu <xzhipeng@umich.edu>
2020-03-04 21:45:06 -0500

    update matrix format

--- matrix/generate.py
+++ matrix/generate.py
@@ -4,10 +4,10 @@ import numpy as np
 LOW = 100 #lower bound of entries
 HIGH = 200 #upper bound of entries
 
-W = 5 #width of matrix
-H = 5 #height of matrix
+W = 3 #width of matrix
+H = W #height of matrix
 
-N = 3 #number of matrix pairs
+N = 1 #number of matrix pairs
 
 
 matrix = np.zeros((2 * N,W,H)).astype(int)
@@ -16,15 +16,23 @@ solutions = np.zeros((N,W,H)).astype(int)
 
 #generate matrix and solutions
 for i in range(0,N):
-    matrix[2 * i] = np.random.randint(HIGH, size = (W,H), dtype=int)
-    matrix[2 * i].tofile("m_{}.bin".format(2 * i))
-    matrix[2 * i + 1] = np.random.randint(HIGH, size = (W,H), dtype=int)
-    matrix[2 * i + 1].tofile("m_{}.bin".format(2 * i + 1))
-    solutions[i] = matrix[2 * i].dot(matrix[2 * i + 1])
-    solutions[i].tofile("s_{}.bin".format(i))
-    solutions[i].tofile("r_{}.bin".format(i)) #r_{} should be generated in verilog
+    matrix[2 * i] = np.random.randint(LOW, HIGH, size = (W,H), dtype=int)
+    a = np.zeros((W + H, H)).astype(int)
+    for j in range(0,W):
+        for k in range(0, H):
+            a[j + k, j] = matrix[2 * i, j, k]
+    np.savetxt("oa_{}.txt".format(i), matrix[2 * i].reshape(W * H, 1), fmt = '%d')
+    np.savetxt("a_{}.txt".format(i), a.reshape((W + H) * H, 1), fmt = '%d')
 
-print(np.fromfile("m_0.bin",  dtype=np.int).reshape(W,H))
-print(np.fromfile("m_1.bin",  dtype=np.int).reshape(W,H))
-print(np.fromfile("s_0.bin",  dtype=np.int).reshape(W,H))
-print(np.fromfile("r_0.bin",  dtype=np.int).reshape(W,H))
+    matrix[2 * i + 1] = np.random.randint(HIGH, size = (W,H), dtype=int)
+    b = np.zeros((W + H, H)).astype(int)
+    for j in range(0,W):
+        for k in range(0, H):
+            b[j + k, j] = matrix[2 * i + 1, k, j]
+    np.savetxt("ob_{}.txt".format(i), matrix[2 * i].reshape(W * H, 1), fmt = '%d')
+    np.savetxt("b_{}.txt".format(i), a.reshape((W + H) * H, 1), fmt = '%d')
+
+    solutions[i] = matrix[2 * i].dot(matrix[2 * i + 1])
+    np.savetxt("s_{}.txt".format(i), solutions[i].reshape(W * H, 1), fmt = '%d')
+    np.savetxt("r_{}.txt".format(i), solutions[i].reshape(W * H, 1), fmt = '%d')
+
