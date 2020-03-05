d7ad38860530381cb7e6cf1097cc842155762921
Zhipeng Xu <xzhipeng@umich.edu>
2020-03-05 15:52:47 -0500

    minor fix

--- matrix/generate.py
+++ matrix/generate.py
@@ -4,7 +4,7 @@ import numpy as np
 LOW = 100 #lower bound of entries
 HIGH = 200 #upper bound of entries
 
-W = 3 #width of matrix
+W = 4 #width of matrix
 H = W #height of matrix
 
 N = 1 #number of matrix pairs
@@ -24,13 +24,13 @@ for i in range(0,N):
     np.savetxt("oa_{}.txt".format(i), matrix[2 * i].reshape(W * H, 1), fmt = '%d')
     np.savetxt("a_{}.txt".format(i), a.reshape((W + H) * H, 1), fmt = '%d')
 
-    matrix[2 * i + 1] = np.random.randint(HIGH, size = (W,H), dtype=int)
+    matrix[2 * i + 1] = np.random.randint(LOW, HIGH, size = (W,H), dtype=int)
     b = np.zeros((W + H, H)).astype(int)
     for j in range(0,W):
         for k in range(0, H):
             b[j + k, j] = matrix[2 * i + 1, k, j]
-    np.savetxt("ob_{}.txt".format(i), matrix[2 * i].reshape(W * H, 1), fmt = '%d')
-    np.savetxt("b_{}.txt".format(i), a.reshape((W + H) * H, 1), fmt = '%d')
+    np.savetxt("ob_{}.txt".format(i), matrix[2 * i + 1].reshape(W * H, 1), fmt = '%d')
+    np.savetxt("b_{}.txt".format(i), b.reshape((W + H) * H, 1), fmt = '%d')
 
     solutions[i] = matrix[2 * i].dot(matrix[2 * i + 1])
     np.savetxt("s_{}.txt".format(i), solutions[i].reshape(W * H, 1), fmt = '%d')
